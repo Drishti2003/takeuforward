1fdf94345e2d69cd8a8389d0279a9b94612d8813
Drishti2003 <s.sainibabli@gmail.com>
2025-01-27 22:14:54 +0530

    basic math

--- Sum_of_all_divisors.java
+++ Sum_of_all_divisors.java
@@ -1,6 +1,6 @@
 public class Sum_of_all_divisors {
     public static void main(String[] args) {
-        System.out.println(sumOfAllDivisors(4));
+        System.out.println(sumOfAllDivisors(3));
     }
 
     // public static int sumOfDivisors(int n){
@@ -10,13 +10,16 @@ public class Sum_of_all_divisors {
     // sum+=i;
     // return sum;
     // }
-
     public static int sumOfAllDivisors(int n) {
+        // Write your code here.
         int sum = 0;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++)
-                if (i % j == 0)
+            for (int j = 1; j * j <= i; j++)
+                if (i % j == 0) {
                     sum += j;
+                    if (i / j != j)
+                        sum += (i / j);
+                }
         }
         return sum;
     }
